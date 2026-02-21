9399a47d86de69e45122fec040215d7dc9be9a32
jhirpara <jhirpara@argusoft.com>
2026-02-21 14:18:03 +0530

    2nd_change

--- fleet-flow-backend/src/main/java/com/backend/fleet_flow_backend/repositories/DriverRepository.java
+++ fleet-flow-backend/src/main/java/com/backend/fleet_flow_backend/repositories/DriverRepository.java
@@ -1,10 +1,18 @@
 package com.backend.fleet_flow_backend.repositories;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.backend.fleet_flow_backend.entities.Driver;
+import com.backend.fleet_flow_backend.entities.DriverStatus;
 
-@RepositoryRestResource(path = "drivers", collectionResourceRel = "drivers")
 public interface DriverRepository extends JpaRepository<Driver, Long> {
+
+    List<Driver> findByStatus(DriverStatus status);
+
+    List<Driver> findByLicenseExpiryBefore(LocalDate date);
+
+    long countByStatus(DriverStatus status);
 }
--- fleet-flow-backend/src/main/java/com/backend/fleet_flow_backend/repositories/VehicleRepository.java
+++ fleet-flow-backend/src/main/java/com/backend/fleet_flow_backend/repositories/VehicleRepository.java
@@ -1,10 +1,24 @@
 package com.backend.fleet_flow_backend.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.jpa.repository.Query;
 
 import com.backend.fleet_flow_backend.entities.Vehicle;
+import com.backend.fleet_flow_backend.entities.VehicleStatus;
+import com.backend.fleet_flow_backend.entities.VehicleType;
 
-@RepositoryRestResource(path = "vehicles", collectionResourceRel = "vehicles")
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
+    List<Vehicle> findByStatus(VehicleStatus status);
+
+    List<Vehicle> findByType(VehicleType type);
+
+    long countByStatus(VehicleStatus status);
+
+    long countByIsRetiredFalse();
+
+    @Query("SELECT v FROM Vehicle v WHERE (:status IS NULL OR v.status = :status) AND (:type IS NULL OR v.type = :type) AND (:region IS NULL OR LOWER(v.region) = LOWER(:region))")
+    List<Vehicle> findFiltered(VehicleStatus status, VehicleType type, String region);
 }
